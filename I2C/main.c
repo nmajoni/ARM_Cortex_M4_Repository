4dafb0bb44c29bdc819e52d280e7632d2a1b9bd1
Nyasha Majoni <ip5jones@gmail.com>
2017-04-25 23:06:24 +0200

    set a couple of more registers, step 4,5 and 6

--- I2C/main.c
+++ I2C/main.c
@@ -1,7 +1,13 @@
-/*Using the I2C bus with the mcp23008 I/O Expander */
-
 
 #include "tm4c_cmsis.h"
+//#include "TM4C123GH6PM.h"
+
+
+
+
+
+
+
 
 
 /*
@@ -23,10 +29,13 @@ int main(void) {
     GPIOA->DEN   = (1U << 6) | (1U << 7); //enable digital functionality
 
 	//4. Enable the I2CSDA pin for open-drain operation. See page 676.
+    GPIOA->ODR  = (1U << 7);  //that's PA7
 
 	//5. Configure the PMCn fields in the GPIOPCTL register to assign the I2C signals to the appropriate pins. See page 688 and Table 23-5 on page 1351.
+    GPIOA->PCTL = (3U << 28) | (3U << 24);   //PMC7 and PMC6 both have the value 3 to choose I2C
 
 	//6. Initialize the I2C Master by writing the I2CMCR register with a value of 0x0000.0010.
+    I2C1_MASTER->MCR = (1U << 4);   //Master mode enabled
 
 	//7. Set the desired SCL clock speed of 100 Kbps by writing the I2CMTPR register with the correct value.
 	/*
@@ -50,3 +59,4 @@ int main(void) {
 
 	return 0;
 }
+
